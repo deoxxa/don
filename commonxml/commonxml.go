5f3af66eb9cb47dd94379fe38dda1acf47d94666
deoxxa <deoxxa@fknsrs.biz>
2017-04-20 20:46:53 +1000

    improve error reporting in commonxml fetch function

--- commonxml/commonxml.go
+++ commonxml/commonxml.go
@@ -10,16 +10,16 @@ import (
 func Fetch(u string, v interface{}) error {
 	res, err := http.Get(u)
 	if err != nil {
-		return errors.Wrap(err, "FetchXML")
+		return errors.Wrap(err, "commonxml.Fetch: couldn't make request")
 	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		return errors.Errorf("FetchXML: invalid status code; expected 200 but got %d", res.StatusCode)
+		return errors.Errorf("commonxml.Fetch: invalid status code; expected 200 but got %d", res.StatusCode)
 	}
 
 	if err := xml.NewDecoder(res.Body).Decode(&v); err != nil {
-		return errors.Wrap(err, "FetchXML")
+		return errors.Wrap(err, "commonxml.Fetch: couldn't decode xml")
 	}
 
 	return nil
